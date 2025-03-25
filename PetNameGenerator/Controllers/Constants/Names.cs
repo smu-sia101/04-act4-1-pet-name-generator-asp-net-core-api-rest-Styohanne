cb5bd82cae8e40b7622e6a6ae1b309a6baed8fab
Styohanne <johannsmain@gmail.com>
2025-03-25 18:07:47 +0800

    final

---
+++ PetNameGenerator/Controllers/Constants/Names.cs
@@ -0,0 +1,35 @@
+﻿namespace PetNameGenerator.Controllers.Constants
+{
+    public static class Names
+    {
+        public static class Dog
+        {
+
+            public static readonly string[] First = new[]
+            { "Buddy", "Max", "Charlie", "Rocky", "Rex","Doug","Sif","Colt","Stan","Ed" };
+
+            public static readonly string[] Last = new[]
+            { "Cooper", "Reynolds", "Barker", "Thompson", "Harrison","Stark","Baratheon","Dayne","Payne","Williams" };
+        }
+
+
+        public static class Cat
+        {
+
+            public static readonly string[] First = new[]
+            { "Whiskers ", "Luna ", "Felix ", "Mittens ", "Jasper","Meowster","Pawna","Palico","Dexter","Jon" };
+            public static readonly string[] Last = new[]
+            { "Montgomery", "Silverman", "Crawford", "Blackwood", "Feline","Stark","Baratheon","Dayne","Payne","Williams" };
+        }
+        public static class Bird
+        {
+
+            public static readonly string[] First = new[]
+            { "Feather", "Sky", "Chirp ", "Wing ", "Plume","Jan","Eri","Highwy","Han","Ren"};
+
+            public static readonly string[] Last = new[]
+            {"Anderson", "Rodriguez", "McQueen", "Sullivan", "Martinez", "Stark", "Baratheon", "Dayne", "Payne", "Williams"};
+        }
+
+    }
+}
---
+++ PetNameGenerator/Controllers/PetNameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using PetNameGenerator.Controllers.Constants;
+
+namespace PetNameGenerator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PetNameController : ControllerBase
+    {
+        [HttpPost("generate")]
+        public IActionResult Post([FromQuery] AnimalType animalType, bool hasLastName)
+        {
+            try 
+            {
+                string firstname = "";
+                string lastname = "";
+                Random random = new Random();
+                int randomIndex = random.Next(0, 10);
+                if (animalType == AnimalType.Dog)
+                {
+                    firstname = Constants.Names.Dog.First[randomIndex];
+                    if (hasLastName)
+                    {
+                        randomIndex = random.Next(0, 10);
+                        lastname = Constants.Names.Dog.Last[randomIndex];
+                    }
+                }
+                else if (animalType == AnimalType.Cat)
+                {
+                    firstname = Constants.Names.Cat.First[randomIndex];
+                    if (hasLastName)
+                    {
+                        randomIndex = random.Next(0, 10);
+                        lastname = Constants.Names.Cat.Last[randomIndex];
+                    }
+                }
+                else if (animalType == AnimalType.Bird)
+                {
+                    firstname = Constants.Names.Bird.First[randomIndex];
+                    if (hasLastName)
+                    {
+                        randomIndex = random.Next(0, 10);
+                        lastname = Constants.Names.Bird.Last[randomIndex];
+                    }
+                }
+                else
+                {
+                    return BadRequest();
+                }
+                return Ok(new { firstname, lastname });
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
